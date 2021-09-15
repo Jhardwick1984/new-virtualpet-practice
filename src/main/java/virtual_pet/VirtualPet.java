d89574ba24d1841e9f380c2a7cbbd517fa2ff8b2
Josh Hardwick <xasummersfallx@msn.com>
2021-09-15 14:47:34 -0400

    pet POJO almost done.

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,21 +1,115 @@
 package virtual_pet;
 
 public class VirtualPet {
-    public String name;
-    public String description;
+    private String name;
+    private String description;
     public int hunger;
     public int thirst;
     public int boredom;
-    public boolean isAlive;
+    private boolean isAlive;
 
-    public VirtualPet(String name, String description, int hunger, int thirst, int boredom) {
+    public VirtualPet(String name, String description) {
         this.name = "Draco";
         this.description = "Not Malfoy.";
-        this.hunger = 50;
-        this.thirst = 50;
-        this.boredom = 50;
+        hunger = 50;
+        thirst = 50;
+        boredom = 50;
         isAlive = true;
     }
 
-    
+    public void greeting() {
+        System.out.println("Welcome to the Dragon Shelter!  Here's some instructions for you: ");
+    }
+
+    public static void giveFood() {
+        if(hunger >= 100) {
+            hunger = 100;
+            System.out.println("My belly is full, choose another action!");
+            System.out.println("");
+        } else {
+            hunger += 15;
+            thirst -= 5;
+            boredom += 10;
+            tick();
+            System.out.println("Om nom, thanks for the food! My hunger is now at " + (hunger+25));
+        }
+    }
+
+    public static void giveWater() {
+        if(thirst >= 100) {
+            thirst = 100;
+            System.out.println("I'm not thirsty!  Do something else!");
+            System.out.println("");
+        } else {
+            hunger -= 5;
+            thirst += 15;
+            boredom += 5;
+            tick();
+            System.out.println("Thanks for the drink!");
+            System.out.println("");
+        }
+    }
+
+    public static void playWith() {
+        if(boredom >= 100) {
+            boredom = 100;
+            System.out.println("I'm entertained enough!  Do something else!");
+            System.out.println("");
+        } else {
+            hunger -= 5;
+            thirst -= 5;
+            boredom += 15;
+            tick();
+            System.out.println("Now that was fun!");
+            System.out.println("");
+        }
+    }
+
+    public static void doNothing() {
+        tick();
+    }
+
+    public void tick() {
+        hunger -= 5;
+        thirst -= 5;
+        boredom -= 5;
+        showHealth();
+    }
+
+    public void showHealth() {
+        System.out.printf("hunger: %s thirst: %s boredom: %s%n", hunger, thirst, boredom);
+        System.out.println("");
+        if(hunger <= 0 || thirst <= 0 || boredom <=0){
+            isAlive = false;
+        }
+    }
+
+    public void exit() {
+        System.out.println("You're leaving?  Very well.");
+        System.out.println("");
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
 }
